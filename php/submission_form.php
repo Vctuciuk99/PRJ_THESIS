ebd035ab41bc71fb77591cfe745ce201ce7e6242
Vctuciuk99 <rdrow589@gmail.com>
2022-05-09 14:52:04 +0800

    ini

--- php/submission_form.php
+++ php/submission_form.php
@@ -1,40 +1,42 @@
 <?php
-//declaration
-$date = filter_input(INPUT_POST,'Date');
-$time_from = filter_input(INPUT_POST, 'Time_from');
-$time_to = filter_input(INPUT_POST, 'Time_to');
-$output = filter_input(INPUT_POST, 'Output');
-$details = filter_input(INPUT_POST, 'Details');
-$verify = filter_input(INPUT_POST, 'Verify');
 
-//submission validation mula dito
-if(empty($date)) {
-    $date_error = "Please enter date";
+//database connection
+$mysqli = require __DIR__ . "/database_conn.php";
+
+//insert new record
+$sql = "INSERT INTO user_diwar_record 
+    (Employee_no, Email, Name, Date, Time_from, Time_to, Output, Details, Verification)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+$stmt = $mysqli->stmt_init();
+
+if(!$stmt->prepare($sql)) {
+    die("SQL error: ". $mysqli->error);
 }
 
-if(empty($time_from)) {
-    $time_from_error = "Please enter time";
+$stmt->bind_param("sssssssss",  
+        $_POST["Employee_no"],
+        $_POST["Email"], 
+        $_POST["Name"], 
+        $_POST["Date"], 
+        $_POST["Time_from"],
+        $_POST["Time_to"], 
+        $_POST["Output"], 
+        $_POST["Details"], 
+        $_POST["Verify"]
+    );
+
+
+if ($stmt->execute()) {
+    //redirect user to page after successfull regisration
+    //header("Location: ../php/acc_home.php");
+    $message = "Report Submitted Successfully";
+    include('../views/acc_home.php');
+    exit;
+
+} else {
+    die($mysqli->error . " " . $mysqli->errno);
 }
-
-if(empty($time_to)) {
-    $time_to_error = "Please enter time";
-}
-
-if(empty($output)) {
-    $output_error = "Please enter output for the day";
-}
-
-if(empty($details)) {
-    $details_error = "Please enter the details";
-}
-
-if(empty($verify)) {
-    $verify_error = "Please enter the verification";
-}
-//hanggang dito
-
-
-include('../views/acc_home.php');
 
 
 
--- views/acc_home.php
+++ views/acc_home.php
@@ -27,54 +27,37 @@
                 <!-- Date -->
                 <label for="Date">DATE: </label>
                 <input type="date" name="Date" required><br>
-                <!-- error msg -->
-                <?php if (isset($date_error)) { ?>
-                    <p><?php echo $date_error ?></p>
-                <?php } ?>
                 
                 <!-- Time From -->
                 <label for="Time_from">TIME FROM: </label>
                 <input type="time" name="Time_from" required><br>
-                <?php if (isset($time_from_error)) { ?>
-                    <p><?php echo $time_from_error ?></p>
-                <?php } ?>
 
                 <!-- Time to -->
                 <label for="Time_to">TIME TO:</label>
                 <input type="time" name="Time_to" required><br>
-                <!-- error msg -->
-                <?php if (isset($time_to_error)) { ?>
-                    <p><?php echo $time_to_error ?></p>
-                <?php } ?>
 
                 <!-- output -->
                 <label for="Output">OUTPUT FOR THE DAY: </label>
                 <input type="text" name="Output" required><br>
-                <!-- error msg -->
-                <?php if (isset($output_error)) { ?>
-                    <p><?php echo $output_error ?></p>
-                <?php } ?>
 
                 <!-- details -->
                 <label for="Details">DETAILS OF THE OUTPUT: </label>
                 <input type="text" name="Details" required><br>
-                <!-- error msg -->
-                <?php if (isset($details_error)) { ?>
-                    <p><?php echo $details_error ?></p>
-                <?php } ?>
                 
                 <!-- verification -->
                 <label for="Verify">VERIFICATION: </label>
                 <input type="text" name="Verify" required><br><br>
-                <!-- error msg -->
-                <?php if (isset($verify_error)) { ?>
-                    <p><?php echo $verify_error ?></p>
-                <?php } ?>
+
                 <button type="submit" name="submit">Submit Form</button><br><br>
                 
+                <!-- submit successfully msg from submisson_form.php-->
+                <?php if (isset($message)) { ?>
+                    <h3><?php echo $message ?></h3>
+                <?php } ?>
+
+
             </form>
             <!--check records-->
-
             <a href="./update_info.php">Update Profile</a><br>
             <br>
 
